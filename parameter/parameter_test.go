f3a96c02f6439fd6138b2bb95c706307d5071da4
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2017-05-20 17:26:38 -0300

    start testing parameter values

---
+++ parameter/parameter_test.go
@@ -0,0 +1,15 @@
+package server_test
+
+import (
+	"testing"
+	//	"github.com/LeoCBS/garden/parameter"
+)
+
+//TODO test body close
+
+func TestParameterFields(t *testing.T) {
+	cases := map[string][]byte{
+		"nameEmpty": []byte{`{value:80.0, "measure":"percent"}`},
+	}
+
+}
