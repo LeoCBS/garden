738dfd24778b1ead645b63f69abe99e85cea297c
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2017-05-21 19:42:04 -0300

    finish main func

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/LeoCBS/garden/parameter"
+	"github.com/LeoCBS/garden/server"
 	"github.com/LeoCBS/garden/storage"
 )
 
 func main() {
 	// TODO get param from cli ou service discovery
-	s := storage.NewStorer("mongodb://localhost:27017", "garden", "parameters")
+	s, err := storage.NewStorage("mongodb://localhost:27017", "garden", "parameters")
+	if err != nil {
+		panic(err)
+	}
 	p := parameter.NewParameter(s)
-	mux := server.NewSever(p)
-	http.ListenAndServe(":3000", mux)
+	mux := server.NewServer(p)
+	sr := &http.Server{
+		Addr:    ":8080",
+		Handler: mux.ServeMux,
+	}
+	log.Fatal(sr.ListenAndServe())
 }
